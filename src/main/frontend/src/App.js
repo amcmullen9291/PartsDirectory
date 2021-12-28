7c372b32a8764003afefbd060df4945475aed503
amcmullen9291 <amcmullen9291@gmail.com>
2021-12-28 17:40:38 -0500

    safety commit

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Welcome from './Components/Welcome';
+import WeeklyAds from './Components/WeeklyAds';
 import DepartmentListings from './Components/DepartmentListings';
 import DetailedDescription from './Components/DetailedDescription';
 import About from './Components/About';
@@ -13,6 +14,7 @@ function App() {
       <Routes>
           <Route path="/" element={<Welcome/>} />
           <Route path="/Parts/About" element={<About />} />
+          <Route path="/Parts/Circular" element={<WeeklyAds />} />
           <Route path="/Parts/departments/:department" element={<DepartmentListings />} />
           <Route path="/Parts/departments/:department/:partsNumber" element={<DetailedDescription />} />
         </Routes>
---
+++ src/main/frontend/src/Components/WeeklyAds.js
@@ -0,0 +1,11 @@
+function WeeklyAds(){
+return (
+    <>
+<div>
+<center><h1>Weekly Circular</h1></center>
+</div>
+   </>
+    )
+}
+
+export default WeeklyAds;
