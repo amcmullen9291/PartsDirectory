503b7c78b891b311f7b68ba88d0fa664f18913c9
amcmullen9291 <amcmullen9291@gmail.com>
2021-12-29 11:56:59 -0500

    safety commit. react-bootstrap bootstrap dependency added

--- src/main/frontend/src/Components/Circular.js
+++ src/main/frontend/src/Components/Circular.js
@@ -1,13 +1,51 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
 function Circular(){
 return (
     <>
 <div>
 <center><h1>Last Week&#39;s Circular</h1></center>
 </div>
-        <center><a href={"/"}>Return to Main Menu</a></center>
-        <br/>
-        <hr/><br/>
+<center><a href={"/"}>Return to Main Menu</a></center>
+<br/>
+<hr/><br/>
 
+ <Carousel fade={true} pause={false}>
+      <Carousel.Item interval={2000}>
+        <img
+          className="d-block w-100"
+          src={`${process.env.PUBLIC_URL}/BarCodes/barCode1.jpg`}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="d-block w-100"
+          src={`${process.env.PUBLIC_URL}/BarCodes/barCode2.jpg`}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img
+          className="d-block w-100"
+          src={`${process.env.PUBLIC_URL}/BarCodes/barCode3.jpg`}
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
    </>
     )
 }
